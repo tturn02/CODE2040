a57a49eeee62ead18ba56881f048f5013893761a
Tracey Turner <tturn02@gmail.com>
2016-10-11 21:28:55 -0400

    Part2/3 Update
    I changed the Githubpost method so that it will now take in some parameters for the JSON object and the uri (because I saw that I was going to be using it frequently). Created a method to reverse strings basically using the Ruby String methods. Also started on part 3

--- Code2040.rb
+++ Code2040.rb
@@ -2,13 +2,11 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-def GithubPost ()
-  uri = URI("http://challenge.code2040.org/api/register")
+def GithubPost (info, uri)
+
 
   header = {'Content-Type' => 'application/json'}
-  info = {         token: '47fca5347f70962e6faac4fae4cb75cd',
-                    github: 'https://github.com/tturn02/CODE2040'
-                        }
+
 
 # Create the HTTP objects
   http = Net::HTTP.new(uri.host, uri.port)
@@ -17,7 +15,33 @@ def GithubPost ()
 
 # Send the request
   response = http.request(request)
-  return response.to_s
+  return response.body
 end
 
-GithubPost()
+def reverse (reverseMe)
+  return reverseMe.reverse
+end
+
+#Part 1
+info = { token: '47fca5347f70962e6faac4fae4cb75cd',
+  github: 'https://github.com/tturn02/CODE2040' }
+
+uri = URI("http://challenge.code2040.org/api/register")
+
+GithubPost(info, uri)
+
+
+#Part2
+info = {token: '47fca5347f70962e6faac4fae4cb75cd'}
+uri = URI("http://challenge.code2040.org/api/reverse")
+stringtoReverse = GithubPost(info, uri)
+reversedString = reverse (stringtoReverse)
+info = {token: '47fca5347f70962e6faac4fae4cb75cd', string: reversedString}
+uri = URI("http://challenge.code2040.org/api/reverse/validate")
+puts GithubPost(info, uri)
+
+
+#Part3
+info = {token: '47fca5347f70962e6faac4fae4cb75cd'}
+uri = URI("http://challenge.code2040.org/api/haystack")
+dictionary =
