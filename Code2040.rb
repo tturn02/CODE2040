14c297a53132e55933dcce78b4acc3623feb6abf
Tracey Turner <tturn02@gmail.com>
2016-10-11 22:53:08 -0400

    Part 4/5
    Was concerned that some strings may contain "=>" which could create potentially create errors given specific string patterns. So, I switched to using JSON's parse method. Then I used Ruby's "start_with" string method to make sure that it's only matching with strings that being with the prefix (using 'contains' could cause potential errors if prefix is located in the middle of a string). Then, I just use an if statement that only adds to the array if it does not have the prefix.

--- Code2040.rb
+++ Code2040.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require 'time'
 
 def GithubPost (info, uri)
 
@@ -28,6 +29,22 @@ def indexed (needHay)
   return haystack.index(needle)
 end
 
+def removePrefix (prefix, arr)
+  removed = Array.new
+  arr.each{ |val|
+    if val.start_with?(prefix) == false
+      removed.push(val)
+    end
+  }
+  return removed
+end
+
+def addTime (timeStamp, seconds)
+ t = Time.parse(timeStamp)
+ t + seconds
+
+end
+
 #Part 1
 info = { token: '47fca5347f70962e6faac4fae4cb75cd',
   github: 'https://github.com/tturn02/CODE2040' }
@@ -50,9 +67,26 @@ uri = URI("http://challenge.code2040.org/api/reverse/validate")
 #Part3
 info = {token: '47fca5347f70962e6faac4fae4cb75cd'}
 uri = URI("http://challenge.code2040.org/api/haystack")
-dictionary = GithubPost(info, uri)
-needleHayStackHash = eval(dictionary.gsub(/:/, '=>'))
+needleHayStackHash = JSON.parse(GithubPost(info, uri))
 index = indexed(needleHayStackHash.flatten)
 info = {token: '47fca5347f70962e6faac4fae4cb75cd', needle: index}
 uri = URI("http://challenge.code2040.org/api/haystack/validate")
-puts GithubPost(info, uri)
+
+
+#Part4
+info = {token: '47fca5347f70962e6faac4fae4cb75cd'}
+uri = URI("http://challenge.code2040.org/api/prefix")
+prefixHash = JSON.parse(GithubPost(info, uri))
+pref = prefixHash["prefix"]
+prefixArray = prefixHash["array"]
+gone = removePrefix(pref, prefixArray)
+info = {token: '47fca5347f70962e6faac4fae4cb75cd', array: gone}
+uri = URI("http://challenge.code2040.org/api/prefix/validate")
+
+#Part5
+info = {token: '47fca5347f70962e6faac4fae4cb75cd'}
+uri = URI("http://challenge.code2040.org/api/dating")
+timeStampHash = JSON.parse(GithubPost(info, uri))
+time = timeStampHash["datestamp"]
+interval = timeStampHash["interval"]
+addTime(time, interval)
